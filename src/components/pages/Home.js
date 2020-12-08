5aeda87735d0047d6701e1f65271881cd0e84f5c
Gustavo Nunes Lucena <gustavo.nunes@codificar.com.br>
2020-12-08 14:38:05 -0300

    Aplicado product grid na home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,6 +10,52 @@ import Grid from "../atoms/Grid"
 import Feature from "../atoms/Feature"
 import Section from "../molecules/Section"
 import Footer from "../organisms/Footer"
+import ProductGrid from "../organisms/ProductGrid"
+
+//Images
+import Driver from "../../assets/driver.jpg"
+import Travel from "../../assets/travel.jpg"
+import Trucks from "../../assets/trucks.jpg"
+import Traffic from "../../assets/traffic.jpg"
+import Bmw from "../../assets/bmw.jpg"
+
+const products = [
+  {
+    id: 1,
+    title: "1ª habilitação de carro e moto",
+    summary:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequaturquidem dolorum voluptatibus et voluptate dolor architecto voluptas!.",
+    media: Traffic,
+  },
+  {
+    id: 2,
+    title: "Habilitação Onibus e caminhão",
+    summary:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequaturquidem dolorum voluptatibus et voluptate dolor architecto voluptas!.",
+    media: Trucks,
+  },
+  {
+    id: 3,
+    title: "Renovação da carteira",
+    summary:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequaturquidem dolorum voluptatibus et voluptate dolor architecto voluptas!.",
+    media: Driver,
+  },
+  {
+    id: 4,
+    title: "Simulados teóricos",
+    summary:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequaturquidem dolorum voluptatibus et voluptate dolor architecto voluptas!.",
+    media: Bmw,
+  },
+  {
+    id: 5,
+    title: "Agendamento de aulas práticas",
+    summary:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequaturquidem dolorum voluptatibus et voluptate dolor architecto voluptas!.",
+    media: Travel,
+  },
+]
 
 const Home = () => (
   <>
@@ -58,6 +104,7 @@ const Home = () => (
       <Heading>
         <h2>Conheça nossos serviços</h2>
       </Heading>
+      <ProductGrid products={products} />
     </Section>
 
     <Section>
